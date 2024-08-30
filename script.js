d714c5dc559b53cf32c0a2aa92c77bc26a0c29f5
SangmalaSuryavanshi <161364128+SangmalaSuryavanshi@users.noreply.github.com>
2024-08-30 21:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+<script>
+function handleSubmit(event) {
+  event.preventDefault(); // This prevents the page from refreshing when the form is submitted
+  alert('Form submitted!');
+}
+</script>
+
+<form id="adoption-form" onsubmit="handleSubmit(event)">
+  ...
+  <!-- rest of your form goes here -->
+  ...
+</form>
